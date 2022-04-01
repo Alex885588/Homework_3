845340c1bb3ca849989b54bdcc686f4201870095
alex <alex.alexanyan55@gmail.com>
2022-04-01 16:50:59 +0400

    Commit

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -17,7 +17,7 @@ public class Tests {
         Assert.assertEquals(rm.printMenu(), "BBQ 3.00$ Pizza 5.00$ ");
     }
     @Test
-    public void removeFoodTest1(){
+    public void removeFoodTest(){
         // when we have food in our menu
         RestaurantMenu rm=new RestaurantMenu();
         Food f1 = new Food("Pizza","5.00$");
@@ -30,34 +30,7 @@ public class Tests {
         Assert.assertTrue(rm.removeFood(f1));
     }
     @Test
-    public void removeFoodTest2(){
-        // when we do not have food in our menu
-        RestaurantMenu rm=new RestaurantMenu();
-        Food f1 = new Food("Pizza","5.00$");
-        Food f2=new Food("Burger","2.20$");
-        Food f3=new Food("Spas","1.20$");
-        Food f4=new Food("Borsh","1.60$");
-        rm.addFood(f1);
-        rm.addFood(f2);
-        rm.addFood(f3);
-        //System.out.println( rm.printMenu());
-        Assert.assertFalse(rm.removeFood(f4));
-    }
-    @Test
-    public void existTest1(){
-        //when it exists
-        RestaurantMenu rm=new RestaurantMenu();
-        Food f1 = new Food("Pizza","5.00$");
-        Food f2=new Food("Burger","2.20$");
-        Food f3=new Food("Spas","1.20$");
-        Food f4=new Food("Borsh","1.60$");
-        rm.addFood(f1);
-        rm.addFood(f2);
-        rm.addFood(f3);
-        Assert.assertTrue(rm.exist(f1));
-    }
-    @Test
-    public void existTest2(){
+    public void existTest(){
         //when it does not exist
         RestaurantMenu rm=new RestaurantMenu();
         Food f1 = new Food("Pizza","5.00$");
